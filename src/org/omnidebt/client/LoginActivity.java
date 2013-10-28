5242fa8e2933e750a54e83e9de71811081b84bc1
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2013-10-28 11:42:59 +0100

    1st version of LoginActivity gui

--- src/org/omnidebt/client/LoginActivity.java
+++ src/org/omnidebt/client/LoginActivity.java
@@ -1,11 +1,31 @@
 package org.omnidebt.client;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.EditText;
+
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.ViewById;
 
 public class LoginActivity extends Activity {
-
+	
+	@ViewById(R.id.text_edit_login)
+	EditText etLogin;
+	
+	@ViewById(R.id.text_edit_password)
+	EditText etPassword;
+	
+	@Click(R.id.button_accept_connection)
+	void onAcceptLoginClicked()
+	{
+		String strLogin		= etLogin.getText().toString();
+		String strPassword	= etPassword.getText().toString();
+		
+		Log.d("login", strLogin + " " + strPassword);
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +38,4 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-
 }
