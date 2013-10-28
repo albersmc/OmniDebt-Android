cb57eaffe1b7ca1ae54ac989caf25c5a7ed25223
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2013-10-28 15:17:49 +0100

    Adding LoginActivity logic, need to add transition between Activities and authentication logic (which is hardcoded at the moment). To log in, use : login : 'test', password : 'pass'.

--- src/org/omnidebt/client/LoginActivity.java
+++ src/org/omnidebt/client/LoginActivity.java
@@ -1,35 +1,39 @@
 package org.omnidebt.client;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
-
-import com.googlecode.androidannotations.annotations.Click;
-import com.googlecode.androidannotations.annotations.ViewById;
 
 public class LoginActivity extends Activity {
 	
-	@ViewById(R.id.text_edit_login)
-	EditText etLogin;
+	EditText	etLogin			= null;
+	EditText	etPassword		= null;
 	
-	@ViewById(R.id.text_edit_password)
-	EditText etPassword;
-	
-	@Click(R.id.button_accept_connection)
-	void onAcceptLoginClicked()
-	{
-		String strLogin		= etLogin.getText().toString();
-		String strPassword	= etPassword.getText().toString();
-		
-		Log.d("login", strLogin + " " + strPassword);
-	}
+	Button		bLogin			= null;
+	Button		bCancel			= null;
+	Button		bCreateAccount	= null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		etLogin			= (EditText)findViewById(R.id.text_edit_login);
+		etPassword		= (EditText)findViewById(R.id.text_edit_password);
+		
+		bLogin			= (Button)	findViewById(R.id.button_accept_connection);
+		bCancel			= (Button)	findViewById(R.id.button_cancel_connection);
+		bCreateAccount	= (Button)	findViewById(R.id.button_create_account);
+		
+		bLogin.			setOnClickListener(onClickLogin);
+		bCancel.		setOnClickListener(onClickCancel);
+		bCreateAccount.	setOnClickListener(onClickCreateAccount);
 	}
 
 	@Override
@@ -38,4 +42,39 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
+
+	// On Login clicked
+	private OnClickListener onClickLogin = new View.OnClickListener() {
+	    @Override
+	    public void onClick(View v) {
+	    	String	strLogin	= etLogin.getText().toString();
+	    	String	strPassword	= etPassword.getText().toString();
+	    	Log.d("login", "Login cliecked : login : " + strLogin + " pass : " + strPassword);
+	    	
+	    	if(strLogin.equals("test") && strPassword.equals("pass")) {
+	    		Log.d("login", "Successfuly authenticated, launching MainODActivity");
+	    		Intent mainActivity = new Intent(v.getContext(), MainODActivity.class);
+	            startActivity(mainActivity);
+	    	}
+	    }
+	};
+	
+	// On Cancel clicked	  
+	private OnClickListener onClickCancel = new View.OnClickListener() {
+	    @Override
+	    public void onClick(View v) {
+	    	etLogin.setText("");
+			etPassword.setText("");
+	    }
+	};
+
+	// On Create Account clicked	  
+	private OnClickListener onClickCreateAccount = new View.OnClickListener() {
+	    @Override
+	    public void onClick(View v) {
+	    	Log.d("login", "Successfuly authenticated, launching MainODActivity");
+    		Intent registerActivity = new Intent(v.getContext(), RegisterActivity.class);
+            startActivity(registerActivity);
+	    }
+	};
 }
