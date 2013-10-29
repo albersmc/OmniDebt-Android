3f84e6b2f4d0b6e411e0cf7c502cbfad42f3751a
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2013-10-29 18:30:35 +0100

    Addind drawer integration with ActionBar

--- src/org/omnidebt/client/view/MainODActivity.java
+++ src/org/omnidebt/client/view/MainODActivity.java
@@ -3,19 +3,21 @@ package org.omnidebt.client.view;
 import org.omnidebt.client.R;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
-import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainODActivity extends Activity {
 
-    private DrawerLayout	dlMainLayout		= null;
-    private String[]		sDrawerContent		= null;
-    private ListView		lvDrawerContainer	= null;
-
+    private DrawerLayout			dlMainLayout		= null;
+    private String[]				sDrawerContent		= null;
+    private ListView				lvDrawerContainer	= null;
+    private ActionBarDrawerToggle	abActionBar			= null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +28,59 @@ public class MainODActivity extends Activity {
         lvDrawerContainer	= (ListView)		findViewById(R.id.drawer_container);
 		sDrawerContent		= (String[])		getResources().getStringArray(R.array.drawer_content);
 
+		abActionBar			= new ActionBarDrawerToggle(this, dlMainLayout, R.drawable.ic_drawer,
+				R.string.drawer_open, R.string.drawer_close) {
+			
+			@Override
+	        public void onDrawerClosed(View view) {
+	            getActionBar().setTitle(R.string.title_activity_main_od);
+	            invalidateOptionsMenu();
+	        }
+
+			@Override
+	        public void onDrawerOpened(View drawerView) {
+	            getActionBar().setTitle(R.string.app_name);
+	            invalidateOptionsMenu();
+	        }
+	        
+		};
+
         // Set the adapter for the list view
         lvDrawerContainer.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, sDrawerContent));
-        
         // Set the list's click listener
-        //lvDrawerContainer.setOnItemClickListener(new DrawerItemClickListener());
+        // TODO find why DrawerItemClickListener is not defined
+        //lvDrawerContainer.setOnItemClickListener(new DrawerItemClickListener());        
 
+		dlMainLayout.setDrawerListener(abActionBar);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
 
 	}
-
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_od, menu);
-		return true;
-	}
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        abActionBar.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        abActionBar.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (abActionBar.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
