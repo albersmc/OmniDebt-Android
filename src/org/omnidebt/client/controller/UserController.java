b37884b29b797fbcd250afb485d9f30d4c7ea65b
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2014-01-21 16:07:57 +0100

    Try HTTPS using OkHttp and KeyStore

--- src/org/omnidebt/client/controller/UserController.java
+++ src/org/omnidebt/client/controller/UserController.java
@@ -1,25 +1,29 @@
 package org.omnidebt.client.controller;
 
-import java.io.IOException;
-import java.net.Proxy;
-import java.net.URL;
-import java.util.List;
+import java.io.InputStream;
+import java.security.KeyStore;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.omnidebt.client.R;
 import org.omnidebt.client.controller.UserConnectCallback.ConnectResponse;
 import org.omnidebt.client.controller.UserSignupCallback.SignupResponse;
 import org.omnidebt.client.view.login.LoginListener;
-import org.omnidebt.client.view.login.LoginListener.ConnectResult;
 import org.omnidebt.client.view.main.Debt;
 import org.omnidebt.client.view.signup.SignUpListener;
-
-import com.squareup.okhttp.OkAuthenticator;
-import com.squareup.okhttp.OkHttpClient;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
 import retrofit.http.POST;
 import retrofit.http.Path;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
 
 public class UserController {
 
@@ -36,48 +40,50 @@ public class UserController {
 		void trySignup(@Path("user") String user, @Path("pass") String pass, @Path("email") String email, Callback<SignupResponse> cb);
 	}
 
-	static public void tryLogin(String login, String passwd, LoginListener callback) {
+	static public void tryLogin(String login, String passwd, LoginListener callback, Context context) {
 
 		strLogin	= login;
 		strPasswd	= passwd;
 
-		// Used to login even without server
-		if(strLogin.equals("test") && strPasswd.equals("pass"))
+
+		try
 		{
-			callback.onConnectResult(ConnectResult.Succeed);
+			KeyStore keyStore = KeyStore.getInstance("BKS");
+			InputStream in = context.getResources().openRawResource(R.raw.mystore);
+			try
+			{
+				keyStore.load(in, "ez24get".toCharArray());
+			} finally
+			{
+				in.close();
+			}
+
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+			tmf.init(keyStore);
+
+			SSLContext sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(null, tmf.getTrustManagers(), null);
+
+			SSLSocketFactory sslFactory = sslContext.getSocketFactory();
+
+			OkHttpClient client=new OkHttpClient()
+				.setSslSocketFactory(sslFactory);
+
+			RestAdapter restAdapter = new RestAdapter.Builder()
+				.setServer("https://88.185.252.7:80")
+				.setClient(new OkClient(client))
+				.build();
+
+			ODLoginService service = restAdapter.create(ODLoginService.class);
+
+			service.tryConnect(strLogin, strPasswd, new UserConnectCallback(callback));
 		}
-		
-		OkHttpClient client=new OkHttpClient();
-		
-		OkAuthenticator authenticator= new OkAuthenticator(){
-
-			@Override
-			public Credential authenticate(Proxy arg0, URL arg1,
-					List<Challenge> arg2) throws IOException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public Credential authenticateProxy(Proxy arg0, URL arg1,
-					List<Challenge> arg2) throws IOException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-		};
-		client.setAuthenticator(authenticator);
-		
-		RestAdapter restAdapter = new RestAdapter.Builder()
-			.setServer("https://88.185.252.7")
-			.setClient(new OkClient(client))
-			.build();
-
-		ODLoginService service = restAdapter.create(ODLoginService.class);
-
-		service.tryConnect(strLogin, strPasswd, new UserConnectCallback(callback));
+		catch(Exception e)
+		{
+			Log.e("https", "fail !");
+		}
 	}
-	
+
 	static public void trySignUp(String strLogin, String strEmail, String strPasswd, String strConfirmPassword, SignUpListener callback) {
 
 		RestAdapter restAdapter = new RestAdapter.Builder()
@@ -88,31 +94,31 @@ public class UserController {
 
 		service.trySignup(strLogin, strPasswd, strEmail, new UserSignupCallback(callback));
 	}
-	
-	
+
+
 	static public Debt[] getDebtList()
 	{
 		Debt[] list=new Debt[]{
-				
-				new Debt("potato","potatu", 0., false),
+
+			new Debt("potato","potatu", 0., false),
 				new Debt("potito","potitu", 0., false),
 				new Debt("poteto","potetu", 0., false),
 				new Debt("pototo","pototu", 0., false),
 		};
-		
+
 		return list;
 	}
-	
+
 	static public Debt[] getDebtHistoric()
 	{
 		Debt[] list=new Debt[]{
-				
-				new Debt("potato","potatu", 0., false),
+
+			new Debt("potato","potatu", 0., false),
 				new Debt("potito","potitu", 0., false),
 				new Debt("poteto","potetu", 0., false),
 				new Debt("pototo","pototu", 0., true),
 		};
-		
+
 		return list;
 	}
 
